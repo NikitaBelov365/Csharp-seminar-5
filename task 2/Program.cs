4e6ae06e29b3a584f8a2334d8c974e2596b5a047
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-26 12:17:56 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 1: Напишите программу замены элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int InputArraySize(string msg)
+{
+    System.Console.WriteLine(msg);
+    if(int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Only integers pls");
+    return 0;
+}
+
+int[] ArrayCreation(int arraySize)
+{
+    int[] array = new int[arraySize];
+    return array;
+}
+
+void ArrayFill(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-9, 9);
+    }
+}
+
+void ArrayPrint(int[] array)
+{
+    System.Console.Write(array[0]);
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        System.Console.Write($", " + array[i]);
+    }
+    System.Console.WriteLine();
+}
+
+void ArrayChange(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+int []array = ArrayCreation(InputArraySize("Input array size"));
+ArrayFill(array);
+ArrayPrint(array);
+ArrayChange(array);
+ArrayPrint(array);
